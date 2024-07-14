00ad5967c85a2395ce517a6cec2a05407ee4f092
Ruben Sepulveda <rsepulveda@planok.com>
2024-07-13 22:44:57 -0400

    avance seccion quienes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,11 +6,32 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             const targetId = this.getAttribute("href").substring(1);
             const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: "smooth"
-            });
+            slowScrollTo(targetElement);
         });
     });
+
+    function slowScrollTo(element) {
+        const targetPosition = element.getBoundingClientRect().top;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition;
+        const duration = 1000; 
+        let start = null;
+
+        window.requestAnimationFrame(step);
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            const progressRate = Math.min(progress / duration, 1); 
+            window.scrollTo(0, startPosition + distance * easeInOutQuad(progressRate));
+            if (progress < duration) {
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        function easeInOutQuad(t) {
+            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        }
+    }
 });
+
